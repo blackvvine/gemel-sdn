ef17d8fb28e6c0f793b8b98d800fe88f893cadc8
KaisongHuang <k86huang@uwaterloo.ca>
2019-04-10 15:54:37 -0400

    update add-instance.sh

--- provision/add-instance.sh
+++ provision/add-instance.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+realpath() {
+    [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
+
 # get current file directory
 DIR="$(realpath $( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd ))"
 
@@ -10,7 +14,16 @@ print_help() {
 }
 
 log() {
-    echo "$(date --rfc-3339="seconds") :: INFO :: $@"
+    echo "$(date)" :: INFO :: $@""
+    # echo "$(date --rfc-3339="seconds") :: INFO :: $@"
+}
+
+SSH() {
+    gcloud compute ssh $1 -- $2
+}
+
+SCP() {
+    gcloud compute scp -r $1 $2
 }
 
 # print help and exit if not enough args given
@@ -25,10 +38,10 @@ SWITCH="$2"
 OVERLAY_IP="$3"
 
 # command aliases for easy unanimous updating
-SSH="ssh -oStrictHostKeyChecking=no"
-SCP="scp -oStrictHostKeyChecking=no"
+# SSH="ssh -oStrictHostKeyChecking=no"
+# SCP="scp -oStrictHostKeyChecking=no"
 
-# create VM 
+# create VM
 log "creating VM $NAME"
 $(realpath $DIR)/create-vm.sh $NAME || exit 1
 
@@ -40,14 +53,14 @@ log "VM external IP address is $VM_IP"
 SWITCH_IP=$(gcloud compute instances list | grep $SWITCH | awk '{ print $5 }')
 log "switch external IP address is $SWITCH_IP"
 log "updating scripts in switch"
-$SCP -r scripts $SWITCH_IP:~ || exit 1
+SCP scripts $NAME:~ || exit 1
 
 # wait until SSH up
 while [ true ]
 do
 
     log "Waiting for VM to boot"
-    $SSH $VM_IP uptime 
+    SSH $NAME uptime
 
     if [ $? -eq 0 ]
     then
@@ -58,15 +71,15 @@ done
 
 # copy ansible playbook to host
 log "updating scripts in VM"
-$SCP -r scripts $VM_IP:~ || exit 1
+SCP scripts $VM_IP:~ || exit 1
 
 # install ansible
-$SSH $VM_IP 'sudo apt-add-repository ppa:ansible/ansible'
-$SSH $VM_IP 'sudo apt update'
-$SSH $VM_IP 'sudo apt install -y ansible'
+SSH $NAME 'sudo apt-add-repository ppa:ansible/ansible'
+SSH $NAME 'sudo apt update'
+SSH $NAME 'sudo apt install -y ansible'
 
 # install OVS
-$SSH $VM_IP 'sudo ansible-playbook scripts/ovs.yml'
+SSH $NAME 'sudo ansible-playbook scripts/ovs.yml'
 
 # generate random VXLAN key
 VXLAN_KEY=$(shuf -i 1-2097151 -n 1)
@@ -78,14 +91,14 @@ SW_INTERNAL_IP=$(gcloud compute instances list | grep "$SWITCH" | awk '{ print $
 log "VM is at $VM_INTERNAL_IP and switch is at $SW_INTERNAL_IP internal VPC address"
 
 log "establishing VM to switch link"
-$SSH $VM_IP "sudo ~/scripts/connect.sh $SW_INTERNAL_IP $VXLAN_KEY"
+SSH $NAME "sudo ~/scripts/connect.sh $SW_INTERNAL_IP $VXLAN_KEY"
 
 log "establishing switch to VM link"
-$SSH $SWITCH_IP "sudo ~/scripts/connect.sh $VM_INTERNAL_IP $VXLAN_KEY"
+SSH $NAME "sudo ~/scripts/connect.sh $VM_INTERNAL_IP $VXLAN_KEY"
 
 # set overlay IP
 log "set VM overlay IP to $OVERLAY_IP"
-$SSH $VM_IP "sudo ~/scripts/set-ip.sh $OVERLAY_IP"
+SSH $NAME "sudo ~/scripts/set-ip.sh $OVERLAY_IP"
 
 
 
