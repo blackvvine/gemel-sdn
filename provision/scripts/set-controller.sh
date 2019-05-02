521a3e691268df216b19e9b02cf40b140283cdf1
Iman Akbari <imakbari@gmail.com>
2019-05-02 15:11:34 -0400

    Fix stupid bug in set-controller.sh

--- provision/scripts/set-controller.sh
+++ provision/scripts/set-controller.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-if [ -z "$1" ]
+if [[ -z ${1+x} ]]
 then
     echo First arg should be controller IP
     exit 1
 fi
 
-ovs-vsctl set bridge br0 protocols=OpenFlow10 -- set-controller br0 tcp:$ip:6633
+ip="$1"
+
+ovs-vsctl set bridge br0 protocols=OpenFlow10 -- set-controller br0 tcp:${ip}:6633
 
 
 
