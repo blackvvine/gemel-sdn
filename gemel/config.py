925d77565993719fe8291e9f69b722d6977d7f00
Iman Akbari <imakbari@gmail.com>
2019-06-24 15:33:56 -0400

    Remove unused GCP info from config

--- gemel/config.py
+++ gemel/config.py
@@ -5,7 +5,5 @@ ODL_VTN_API_URL = f"{ODL_BASE_URL}:8181/restconf/operations"
 
 ODL_MAIN_API_URL = f"{ODL_BASE_URL}:8080/restconf/operational"
 
-GCP_ACCOUNT_NAME = "919881239930-compute@developer.gserviceaccount.com"
-GCP_KEY_FILE = "/home/iakbaria/Downloads/phdandpeasant-d49400a1f9bc.json"
 
 
